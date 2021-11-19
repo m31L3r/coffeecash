fe89fe2dc59e37c26b4728f458e103881659e94a
Michael Meiler <michael-meiler@gmx.de>
2021-11-19 23:52:59 +0100

    csv read to dictonary and list

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ from kivy.uix.stacklayout import StackLayout
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 
+Drinkers = []
+
 class BoxLayoutExample(BoxLayout):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         self.orientation = "vertical"
-        csv_read_obj = csv.reader(open('coffee_table.csv'))
+        with open('coffee_table.csv', mode='r') as csv_file:
+            csv_read_obj = csv.DictReader(csv_file)
+            for row in csv_read_obj:
+                Drinkers.append(row["lastname"])
 
-        for row in csv_read_obj:
+        for drinker in Drinkers:
             x = dp(300)
             y = dp(100)
-            b = Button(text = str(row[0]), size_hint=(None,None), size=(x,y), font_size=24)
+            b = Button(text = drinker, size_hint=(None,None), size=(x,y), font_size=24)
             self.add_widget(b)
 
 class BoxLayoutExample(BoxLayout):
